2d071579cc1c6b9116dc702391b1e07f71d6a529
Aadil-7 <aadilbhusnur007@gmail.com>
2022-09-30 20:22:51 +0530

    added a message for landscape mode.

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const newQuote = () => {
   fetch("https://type.fit/api/quotes")
     .then(response => response.json())
     .then((data) => {
+
       let quoteData = data[Math.ceil(Math.random() * 1643)]
       let quote = quoteData.text
       let author = quoteData.author
@@ -13,5 +14,39 @@ const newQuote = () => {
       document.getElementById(`author`).innerHTML = `Something Went Wrong <br>Check Your Connection.`
     });
 }
+
 newQuote();
-window.scrollBy(0,100)
+
+window.addEventListener("orientationchange", function () {
+  oriDetection();
+});
+
+let intervalId
+let timeoutId
+let secs = 5
+function oriDetection() {
+  if (window.orientation == 90) {
+    document.getElementsByClassName(`container`)[0].classList.add(`displayNone`)
+    document.getElementsByClassName(`rotateMess`)[0].classList.add(`displayBlock`)
+    intervalId = setInterval(() => {
+      document.getElementById(`timer`).innerText = `${secs}`
+      secs -= 1
+      if (secs < 1) { secs = 5 }
+    }, 1000);
+    timeoutId = setTimeout(autoDisplay, 6000)
+  } else if (window.orientation == 0) {
+    document.getElementsByClassName(`container`)[0].classList.remove(`displayNone`)
+    document.getElementsByClassName(`rotateMess`)[0].classList.remove(`displayBlock`)
+    clearInterval(intervalId)
+    clearTimeout(timeoutId)
+    secs = 5
+  }
+}
+
+function autoDisplay() {
+  document.getElementsByClassName(`container`)[0].classList.remove(`displayNone`)
+  document.getElementsByClassName(`rotateMess`)[0].classList.remove(`displayBlock`)
+  clearInterval(intervalId)
+  clearTimeout(timeoutId)
+  secs = 5
+}
