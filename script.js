40fecce71fde1b797e7ad1c2f686e81cd47bf9e9
Aadil-7 <aadilbhusnur007@gmail.com>
2022-10-01 18:22:00 +0530

    made code efficient using functions and also added 270 degree rotate message

--- script.js
+++ script.js
@@ -25,20 +25,11 @@ let timeoutId
 let secs = 5
 function oriDetection() {
   if (window.orientation == 90) {
-    document.getElementsByClassName(`container`)[0].classList.add(`displayNone`)
-    document.getElementsByClassName(`rotateMess`)[0].classList.add(`displayBlock`)
-    intervalId = setInterval(() => {
-      document.getElementById(`timer`).innerText = `${secs}`
-      secs -= 1
-      if (secs < 1) { secs = 5 }
-    }, 1000);
-    timeoutId = setTimeout(autoDisplay, 6000)
+    rotateDisplay()
+  } else if (window.orientation == 270) {
+    rotateDisplay()
   } else if (window.orientation == 0) {
-    document.getElementsByClassName(`container`)[0].classList.remove(`displayNone`)
-    document.getElementsByClassName(`rotateMess`)[0].classList.remove(`displayBlock`)
-    clearInterval(intervalId)
-    clearTimeout(timeoutId)
-    secs = 5
+    autoDisplay()
   }
 }
 
@@ -48,5 +39,16 @@ function autoDisplay() {
   clearInterval(intervalId)
   clearTimeout(timeoutId)
   secs = 5
+}
+
+function rotateDisplay(){
+  document.getElementsByClassName(`container`)[0].classList.add(`displayNone`)
+    document.getElementsByClassName(`rotateMess`)[0].classList.add(`displayBlock`)
+    intervalId = setInterval(() => {
+      document.getElementById(`timer`).innerText = `${secs}`
+      secs -= 1
+      if (secs < 1) { secs = 5 }
+    }, 1000);
+    timeoutId = setTimeout(autoDisplay, 6000)
 }
 oriDetection();
